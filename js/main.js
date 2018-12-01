18ca837f6c3c2f040bf9ddc07a3d1e8ad304cae2
Bouchiha Abdelrahim <b.abdelrahim@outlook.fr>
2018-12-01 02:01:49 +0100

    Les fichier de base pour commencé
    ajout des fichier suivant :
    +style.css
    +index.html
    +main.js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+window.onload = main;
+
+class joueur{
+	#name;
+	#size = 5;
+	#positionDeDepare = {
+		x;
+		y;
+	}
+
+	constructor (name, x, y){
+		this.#name = name;
+		this.#positionDeDepare.x = x;
+		this.#positionDeDepare.y = y;
+	}
+
+
+}
+
+function main() {
+	
+	
+}
