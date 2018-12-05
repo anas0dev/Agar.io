1c4ea16a25157d820047c87cd8107d0fbdff13e9
Unknown <b.abdelrahim@outlook.fr>
2018-12-05 03:59:54 +0100

    Amélioration de la classe joueur et mise en place de la map
    dans la classe joueur 
    ajout d'attribut :
    +color
    +vitesse
    +context
    +positionX   a la place de position.x
    +positionY   a la place de position.x
    
    ajout des getter & setter de :
    name, size, vitesse
    
    methode :
    +draw()
    
    Pour la map 
    creation de la methode :
    +createMap()
    
    en plus les methodes suivante :
    +random(min, max) 
    +randomColorHex()
    +log(message)

--- js/main.js
+++ js/main.js
@@ -1,23 +1,84 @@
 window.onload = main;
 
+var w = window.innerWidth;
+var h = window.innerHeight;
+
 class joueur{
-	#name;
-	#size = 5;
-	#positionDeDepare = {
+	/*name;
+	size = 5;
+	positionDeDepare = {
 		x;
 		y;
+	}*/
+
+	constructor (name, context){
+		this.name = name;
+		this.color = randomColorHex();
+		this.size = 15;
+		this.vitesse = 2;
+		this.positionX = random(1, w);
+		this.positionY = random(1, h);
+		this.context = context;
+		//draw();
 	}
 
-	constructor (name, x, y){
-		this.#name = name;
-		this.#positionDeDepare.x = x;
-		this.#positionDeDepare.y = y;
+	draw(){
+		this.context.beginPath();
+		this.context.fillStyle = this.color;
+		this.context.arc(this.positionX, this.positionY, this.size, 0, 2 * Math.PI);
+		this.context.fill();
 	}
 
+	get getName(){
+		return this.name;
+	}
+	set setName(newName){
+		this.name = newName;
+	}
 
+	get getSize(){
+		return this.size;
+	}
+	set setSize(newSize){
+		this.size = newSize;
+	}
+
+	get getVitesse(){
+		return this.vitesse;
+	}
+	set setVitesse(newVitesse){
+		this.vitesse = newVitesse;
+	}
+
+}
+
+function log(message){
+	console.log(message);
+}
+
+function randomColorHex(){
+	return '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+}
+
+function random(min, max){
+	return Math.floor((Math.random() * (max - min)) + min);
+}
+
+function createMap(){
+	let map = document.createElement("canvas");
+	map.width = w;
+	map.height = h;
+	document.body.appendChild(map);
+	return map;
 }
 
 function main() {
 	
+	var map = createMap();
+	var context = map.getContext("2d");
+
+	var anas = new joueur("anas", context);
+	anas.draw();
+
 	
 }
