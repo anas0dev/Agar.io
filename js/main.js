33996741ab3630e6941cb76938e83ef9cde1eb20
Abdelrahim <b.abdelrahim@outlook.fr>
2018-12-17 06:57:02 +0100

    Creation d'une nouvelle classe Vector qui contient des méthode pour manipuler les vecteur, + quelque modif sur la classe joueur

--- js/main.js
+++ js/main.js
@@ -3,6 +3,55 @@ window.onload = main;
 var w = window.innerWidth;
 var h = window.innerHeight;
 
+class Vector{
+	constructor (x, y, length){
+		this.x = x;
+		this.y = y;
+		if(length)
+			this.length = length;
+		else
+			this.length = this.lengthVector();
+	}
+	
+	set setVector(v){
+		this.x = v.x;
+		this.y = v.y;
+		this.length = v.length;
+	}
+	get getVector(){
+		return this;
+	}
+	
+	get getLength(){
+		return this.length;
+	}
+	setLength(length){
+		this.length = length;
+	}
+	
+	add(v){
+		this.x += v.x;
+		this.y += v.y;
+		return this;
+	}
+	
+	sub(v){
+		this.x -= v.x;
+		this.y -= v.y;
+		return this;
+	}
+	
+	copy(){
+		return new Vector(this.x, this.y);
+	}
+	
+	lengthVector(){
+		return Math.sqrt(this.x * this.x + this.y * this.y);
+	}
+	
+	
+}
+
 class joueur{
 	/*name;
 	size = 5;
@@ -16,18 +65,25 @@ class joueur{
 		this.color = randomColorHex();
 		this.size = 15;
 		this.vitesse = 2;
-		this.positionX = random(1, w);
-		this.positionY = random(1, h);
+		this.position = new Vector(random(1, w), random(1, h));
 		this.context = context;
-		//draw();
+		//this.mouse = new MouseEvent("mouse");
+		this.draw();
 	}
 
 	draw(){
 		this.context.beginPath();
 		this.context.fillStyle = this.color;
-		this.context.arc(this.positionX, this.positionY, this.size, 0, 2 * Math.PI);
+		this.context.arc(this.position.x, this.position.y, this.size, 0, 2 * Math.PI);
 		this.context.fill();
 	}
+	
+	/*this.addEventListener("mousemove", function(e){
+		let mouse = new Vector(e.clientX, e.clientY);
+		mouse.sub(this.position);
+		mouse.setLength(5);
+		this.position.add(mouse);
+	});*/
 
 	get getName(){
 		return this.name;
@@ -78,7 +134,14 @@ function main() {
 	var context = map.getContext("2d");
 
 	var anas = new joueur("anas", context);
-	anas.draw();
+	map.addEventListener("mousemove", function(e){
+		let mouse = new Vector(e.clientX, e.clientY);
+		mouse.sub(anas.position);
+		mouse.setLength(5);
+		anas.position.add(mouse);
+		anas.draw();
+	});
+	//anas.draw();
 
 	
 }
