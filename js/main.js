6edc77ac9ffe7c4fc55c9ad44316ab57a5520219
Abdelrahim <b.abdelrahim@outlook.fr>
2018-12-29 05:29:42 +0100

    ajout de la mass + quelque modif
    ajout de l'attribut mass dans la classe circle (j'ai renommer tout les mot cercle en circle et aussi size par radius) avec ces getter et setter et aussi la methode calculateMass()
    
    modification aussi pour la classe joueur qui est devenu player en modifiant eatCircle(c)
    
    tout ça pour rendre la progression du joueur plus logique

--- js/main.js
+++ js/main.js
@@ -23,10 +23,7 @@ var context;
 var moving = false;
 var distMouse;
 
-document.addEventListener("mousemove", function(e){
-	mouse = new Vector(e.clientX, e.clientY);
-	moving = true;
-});
+
 
 function main() {
 
@@ -34,77 +31,32 @@ function main() {
 	context = map.getContext("2d");
 	
 	for(let i = 0; i < 100; i++){
-		foods[i] = new Cercle();
+		foods[i] = new Circle();
 	}
 
 	player = new Joueur("anas");
-	/* window.addEventListener("mousemove", function(e){
-		mouse = new Vector(e.clientX, e.clientY);
-		clearArc(player.position.x, player.position.y, player.size+1);
 
-		mouse.sub(player.position);
-		mouse.setLength(5);
-		player.position.add(mouse.div(player.vitesse));
-		//player.draw();
-	}); */
-	//player.draw();
 	window.requestAnimationFrame(onFrame);
 	
 }
-// var a = 1;
+
 function onFrame(){
-	//let mouseTmp = mouse;
 	frameLoop = window.requestAnimationFrame(onFrame);
 	drawGrid();
 	for(let i = 0; i < foods.length; i++){
-		drawCercle(foods[i]);
+		drawCircle(foods[i]);
 	}
-	clearArc(player.position.x, player.position.y, player.size);
-	/* if(mouse !== undefined && mouse !== player.position){
-		if(moving){
-			mouseTmp.sub(player.position);
-			player.position.add(mouseTmp.div(player.vitesse).round());
-			moving = false;
-			
-		}else{
-			player.position.add(mouseTmp);
-			//log("2");
-		}
-	} */
+	clearArc(player.position.x, player.position.y, player.radius+1);
+
 	player.move();
 	
 	for(let i = 0; i < foods.length; i++){
-		log(player.distanceToCercle(foods[i]));
-		if(player.distanceToCercle(foods[i]) < player.getSize + foods[i].getSize){
-			player.eatCercle(foods[i]);
+		if(player.distanceToCircle(foods[i]) < player.getRadius - 2){
+			player.eatCircle(foods[i]);
+			clearArc(foods[i].position.x, foods[i].position.y, foods[i].radius+1);
 			foods.splice(i, 1);
 		}
 	}
-	// hits(player);
 	drawPlayer(player);
 }
-
-/* function drawGrid(){
-	// let nombreGridWidth = w / 10;
-	// let nombreGridHeight = h / 10;
-	
-	context.lineWidth = 1;
-	context.strokeStyle = "#dddddd";
-	context.globalAlpha = 0.1;
-	context.beginPath();
-	
-	for(let i=50; i < w; i += 50){
-		context.moveTo(i, 0);
-		context.lineTo(i, h);
-	}
-	
-	for(let i=50; i < h; i += 50){
-		context.moveTo(0, i);
-		context.lineTo(w, i);
-	}
-	
-	context.stroke();
-	context.globalAlpha = 1;
-} */
-
 
